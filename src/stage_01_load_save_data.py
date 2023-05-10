79100f6ea7b1f93686f7916d029cabf6967a32a3
Sukruth097 <sukruthav007@gmail.com>
2023-05-10 23:00:42 +0530

    stage_01 modified

--- src/stage_01_load_save_data.py
+++ src/stage_01_load_save_data.py
@@ -12,9 +12,7 @@ STAGE = "DATA_INGESTION"
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
     level=logging.INFO, 
-    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-    filemode="a"
-    )
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",filemode="a")
 
 
 def get_data(config_path):
